aac6ac80c63924c6659236f89e57ca9d3344be8b
a-akin <46995386+a-akin@users.noreply.github.com>
2019-03-07 23:13:49 +0000

    Added the Movie, Genre, Actor, Director class.

---
+++ DMUViews/DMUViews/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DMUViews.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string ImagePath { get; set; }
+        public string TrailerPath { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReleased { get; set; }
+    }
+}
