e367d55fa1f301a9f48b06ac9c75e02a59216cc5
Abdul Sharif <P17229859@my365.dmu.ac.uk>
2019-03-12 11:04:04 +0000

    new file

---
+++ DMUView/DMUView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DMUView.Startup))]
+namespace DMUView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
